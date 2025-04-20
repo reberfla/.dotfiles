e80ff1b228a5a9baca44ceeb05297aeda74969ec
Flavio Reber <reber.flavio@gmail.com>
2025-04-20 17:40:09 +0200

    migrate everything to lazy exept dap (which i'm not sure if I want to use it)

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,39 @@
+return {
+    "epwalsh/obsidian.nvim",
+    config = function()
+        require("obsidian").setup({
+            workspaces = {
+                {
+                    name = "Notes",
+                    path = "~/notes",
+                },
+            },
+            new_notes_location = "notes_subdir",
+            ---@param title string|?
+            ---@return string
+            note_id_func = function(title)
+                local suffix = ""
+                if title ~= nil then
+                    suffix = title:gsub(" ", "-"):lower()
+                else
+                    for _ = 1, 4 do
+                        suffix = suffix .. string.char(math.random(65, 90))
+                    end
+                    suffix = tostring(os.time()) .. "-" .. suffix
+                end
+                return suffix
+            end,
+            templates = {
+                folder = "~/.config/obsidian/templates",
+                date_formate = "%Y-$m-%d",
+                time_format = " %H:%M"
+            },
+            ui = {
+                hl_groups = require('rose-pine.plugins.obsidian')
+            },
+            follow_url_func = function(url)
+                vim.ui.open(url)
+            end
+        })
+    end
+}

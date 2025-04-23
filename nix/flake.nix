37047091f65f62c754b219b9f53cdb02d8ab35e3
Flavio Reber <reber.flavio@gmail.com>
2025-04-23 22:26:20 +0200

    add nix config, update install with stow to have right path for nix config

---
+++ nix/flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "My MacOs config flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile.
+      environment.systemPackages =
+        [ pkgs.vim
+          pkgs.coursier
+          pkgs.fzf
+          pkgs.gradle
+          pkgs.graphviz
+          pkgs.jq
+          pkgs.lazydocker
+          pkgs.lazygit
+          pkgs.lima
+          pkgs.nmap
+          pkgs.neovim
+          pkgs.nodejs_23
+          pkgs.pandoc
+          pkgs.qemu
+          pkgs.ripgrep
+          pkgs.starship
+          pkgs.stow
+          pkgs.tmux
+          pkgs.tree
+          pkgs.zoxide
+          pkgs.zsh
+          # pkgs.zsh-autosuggestions
+          # pkgs.zsh-syntax-highlighting
+          pkgs.xh
+        ];
+
+      nix.settings.experimental-features = "nix-command flakes";
+      programs.zsh.enable = true;
+      programs.zsh.enableSyntaxHighlighting = true;
+      programs.zsh.enableCompletion = true;
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 5;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      security.pam.enableSudoTouchIdAuth = true;
+
+      # MacOs System Settings
+      system.defaults = {
+        dock = {
+          autohide = true;
+          mru-spaces = false;
+          show-recents = false;
+          static-only = true;
+        };
+        finder = {
+          AppleShowAllExtensions = true;
+          ShowPathbar = true;
+          FXPreferredViewStyle = "clmv";
+          };
+        screencapture.location = "~/Pictures/screenshots";
+        screensaver.askForPasswordDelay = 10;
+      };
+
+      # Homebrew Casks and Taps
+      homebrew = {
+          enable = true;
+          brews = [
+            "zsh-autosuggestions"
+            "zsh-syntax-highlighting"
+          ];
+          casks = [
+              "1password"
+              "dbeaver-community"
+              "google-chrome"
+              "postman"
+              "wezterm"
+              "obsidian"
+              "docker"
+            ];
+          taps = [
+              "sdkman/tap"
+            ];
+        };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#MacbookPro-2
+    darwinConfigurations."Flavio-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ configuration ];
+    };
+  };
+}

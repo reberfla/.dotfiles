b3fede099c86f0dd28ebb3345a8e77c197245f3a
Flavio Reber <reber.flavio@gmail.com>
2025-04-24 16:08:57 +0200

    remove zsh settings wich didn't work

--- nix/flake.nix
+++ nix/flake.nix
@@ -40,8 +40,6 @@
 
       nix.settings.experimental-features = "nix-command flakes";
       programs.zsh.enable = true;
-      programs.zsh.enableSyntaxHighlighting = true;
-      programs.zsh.enableCompletion = true;
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.stateVersion = 5;
       nixpkgs.hostPlatform = "aarch64-darwin";

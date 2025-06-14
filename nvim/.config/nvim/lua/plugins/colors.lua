ff4fffa34b32cf01650ce83501d8ee03307d0d25
Flavio Reber <reber.flavio@gmail.com>
2025-06-14 17:12:57 +0200

    remove border bg

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -4,8 +4,10 @@ return {
     config = function()
         vim.cmd("colorscheme rose-pine")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
 
         require("rose-pine").setup({
             highlight_groups = {

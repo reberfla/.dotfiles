2727113ddc8cdbeb82e652348b7de47cbd12c11e
Flavio Reber <reber.flavio@gmail.com>
2025-09-10 21:57:13 +0200

    split private/work setup

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,39 +9,60 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile.
-      environment.systemPackages =
-        [ pkgs.vim
-          pkgs.azure-cli
-          pkgs.fzf
-          pkgs.graphviz
-          pkgs.jq
-          pkgs.lazydocker
-          pkgs.lazygit
-          pkgs.lima
-          pkgs.nmap
-          pkgs.nodejs_22
-          pkgs.pandoc
-          pkgs.pngpaste
-          pkgs.qemu
-          pkgs.ripgrep
-          pkgs.starship
-          pkgs.stow
-          pkgs.tmux
-          pkgs.tree
-          pkgs.uv
-          pkgs.zoxide
-          pkgs.zsh
-          pkgs.xh
-        ];
+      commonPackages = pkgs: [
+        pkgs.fzf
+        pkgs.graphviz
+        pkgs.jq
+        pkgs.lazydocker
+        pkgs.lazygit
+        pkgs.nmap
+        pkgs.pngpaste
+        pkgs.ripgrep
+        pkgs.starship
+        pkgs.stow
+        pkgs.tmux
+        pkgs.tree
+        pkgs.uv
+        pkgs.zoxide
+        pkgs.zsh
+        pkgs.xh
+      ];
 
+    commonBrews = [
+          "zsh-autosuggestions"
+          "zsh-syntax-highlighting"
+          "neovim"
+          "sdkman/tap/sdkman-cli"
+        ];
+    commonCasks = [
+        "intellij-idea"
+        "google-chrome"
+        "postman"
+        "wezterm"
+        "obsidian"
+        "docker"
+        "spotify"
+      ];
+
+    commonTaps = [
+        "sdkman/tap"
+      ];
+
+    commonDarwinModule = { pkgs, ... }: {
       nix.settings.experimental-features = "nix-command flakes";
       programs.zsh.enable = true;
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.stateVersion = 5;
       nixpkgs.hostPlatform = "aarch64-darwin";
       security.pam.enableSudoTouchIdAuth = true;
+
+      environment.systemPackages = commonPackages pkgs;
+      homebrew = {
+        enable = true;
+        brews = commonBrews;
+        taps = commonTaps;
+        casks = commonCasks;
+      };
 
       # MacOs System Settings
       system.defaults = {
@@ -75,43 +96,43 @@
           enableKeyMapping = true;
           swapLeftCommandAndLeftAlt = false;
         };
-
-      # Homebrew Casks and Taps
-      homebrew = {
-          enable = true;
-          brews = [
-            "zsh-autosuggestions"
-            "zsh-syntax-highlighting"
-            "neovim"
-            "sdkman/tap/sdkman-cli"
-          ];
-          casks = [
-              "1password"
-              "dbeaver-community"
-              "intellij-idea"
-              "google-chrome"
-              "postman"
-              "wezterm"
-              "obsidian"
-              "docker"
-              "spotify"
-              "slack"
-              "basictex"
-            ];
-          taps = [
-              "sdkman/tap"
-            ];
-        };
     };
   in
   {
     darwinConfigurations."Flavio-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-      modules = [ configuration ];
+      modules = [ 
+        commonDarwinModule
+        ({ pkgs, ... }: {
+            environment.systemPackages = [
+              pkgs.azure-cli
+              pkgs.lima
+              pkgs.nmap
+              pkgs.nodejs_22
+              pkgs.pandoc
+              pkgs.qemu
+            ];
+            homebrew.casks = [
+                "1password"
+                "dbeaver-community"
+                "obsidian"
+                "basictex"
+                "dotnet-sdk"
+              ];
+          })];
     };
     darwinConfigurations."DTCHZURIB302232" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-      modules = [ configuration ];
+      modules = [ 
+        commonDarwinModule
+        ({ pkgs, ...}: {
+            environment.systemPackages = [
+              pkgs.awscli2
+            ];
+            homebrew.casks = [
+                "slack"
+              ];
+      })];
     };
   };
 }
